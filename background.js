252dae99b43431321cdc6afc2604553c5c619e54
Ignacio Bergmann <ignacio_bergmann@discovery.com>
2018-01-10 12:30:12 +0000

    Basic version working for 6.3

--- background.js
+++ background.js
@@ -9,14 +9,39 @@ var AuthoringUIMode = {
   TOUCH : "touch"
 };
 
-function checkMode(url) {
-  if (url.indexOf('/cf') == -1  && url.indexOf('/editor.html') == -1 && url.indexOf('wcmmode=disabled') != -1) {
-    return Mode.RED;
-  } else if (url.indexOf('/cf') == -1  && url.indexOf('/editor.html') == -1 && url.indexOf('wcmmode=disabled') == -1) {
-    return Mode.YELLOW;
+function checkMode(url, callback) {
+  if (url.indexOf('/editor.html') == -1) {
+    callback(Mode.RED);
+    console.log("RED");
   } else {
-    return Mode.GREEN;
+    chrome.cookies.get({"url": url, "name": "cq-editor-layer.page"}, function(cookie) {
+      if (cookie != null) {
+        if (cookie.value == "Edit") {
+          callback(Mode.GREEN);
+          console.log("GREEN");
+        } else if (cookie.value == "Preview") {
+          callback(Mode.YELLOW);
+          console.log("YELLOW");
+        } else {
+          callback(Mode.RED);
+          console.log("RED: " + cookie.value);
+        }
+      } else {
+        chrome.cookies.get({"url": url, "name": "wcmmode"}, function(cookie) {
+          if (cookie != null) {
+            console.log(cookie.value);
+          }
+        });
+      }
+    });
   }
+//   if (url.indexOf('/cf') == -1  && url.indexOf('/editor.html') == -1 && url.indexOf('wcmmode=disabled') != -1) {
+//     return Mode.RED;
+//   } else if (url.indexOf('/cf') == -1  && url.indexOf('/editor.html') == -1 && url.indexOf('wcmmode=disabled') == -1) {
+//     return Mode.YELLOW;
+//   } else {
+//     return Mode.GREEN;
+//   }
 }
 
 function getAuthoringUIModeKey(tabId) {
@@ -51,72 +76,102 @@ chrome.webNavigation.onCompleted.addListener(
     if (details.frameId == 0) {
       var tabId = details.tabId;
       var url = details.url;
-      var mode = checkMode(url);
-      setPageActionIconToMode(tabId, mode);
-      var title;
-      if (mode == Mode.RED) {
-        title = "AEM page (CF and WCM is disabled)";
-      } else if (mode == Mode.YELLOW) {
-        title = "AEM page (CF is disabled)";
-      } else if (mode == Mode.GREEN) {
-        title = "AEM page";
-        var authoringUIMode = AuthoringUIMode.TOUCH;
-        if (url.indexOf('/editor.html') == -1) {
-          authoringUIMode = AuthoringUIMode.CLASSIC;
+      setupPageAction(tabId, url);
+      chrome.cookies.onChanged.addListener(function(changeInfo) {
+        if (changeInfo.cause == "explicit" && changeInfo.cookie.name == "cq-editor-layer.page") {
+          setupPageAction(tabId, url);
         }
-        var value = {};
-        value[getAuthoringUIModeKey(tabId)] = authoringUIMode;
-        chrome.storage.local.set(value);
-        
-      }
-      setPageActionTitle(tabId, title);
+      });
     }
   },
   { url: [{ pathPrefix: '/cf' }, { pathPrefix: '/content' }, { pathPrefix: '/editor.html' }] }
 );
 
+function setupPageAction(tabId, url) {
+  checkMode(url, function(mode) {
+    setPageActionIconToMode(tabId, mode);
+    var title;
+    if (mode == Mode.RED) {
+      title = "AEM page (CF and WCM is disabled)";
+    } else if (mode == Mode.YELLOW) {
+      title = "AEM page (CF is disabled)";
+    } else if (mode == Mode.GREEN) {
+      title = "AEM page";
+      var authoringUIMode = AuthoringUIMode.TOUCH;
+      if (url.indexOf('/editor.html') == -1) {
+        authoringUIMode = AuthoringUIMode.CLASSIC;
+      }
+      var value = {};
+      value[getAuthoringUIModeKey(tabId)] = authoringUIMode;
+      chrome.storage.local.set(value);
+    
+    }
+    setPageActionTitle(tabId, title);
+  });
+}
+
 chrome.pageAction.onClicked.addListener(function(tab) {
   var tabId = tab.id;
-  var url = tab.url;
+  var url =  tab.url;
   setPageActionIconToMode(tabId);
   setPageActionTitle(tabId, "Loading AEM page...");
-  var mode = checkMode(url);
-  if (mode == Mode.RED) {
-    var key = getAuthoringUIModeKey(tabId);
-    chrome.storage.sync.get({
-      defaultAuthoringUIMode: AuthoringUIMode.CLASSIC
-    }, function(items) {
-      var defaultAuthoringUIMode = items.defaultAuthoringUIMode;
-      chrome.storage.local.get(key, function(items) {
-        var authoringUIMode = items[key];
-        if (authoringUIMode == undefined) {
-          authoringUIMode = defaultAuthoringUIMode;
-        }
-        var prefix = '/editor.html/content';
-        if (authoringUIMode != AuthoringUIMode.TOUCH) {
-          prefix = '/cf#/content';
-        }
-        url = url.replace('/content', prefix);
-        url = url.replace(/&wcmmode=disabled|\?wcmmode=disabled/gi, '');
+  var mode = checkMode(url, function(mode) {
+    if (mode == Mode.RED) {
+      var prefix = '/editor.html/content';
+      url = url.replace('/content', prefix);
+      url = url.replace(/&wcmmode=disabled|\?wcmmode=disabled/gi, '');
+      chrome.cookies.set({"url": url.split("editor.html")[0], "name": "cq-editor-layer.page", "value": "Edit"}, function(cookie) {
         chrome.tabs.update(tabId, { url: url });
       });
-    });
-  } else if (mode == Mode.YELLOW) {
-    if(url.indexOf('?') == -1) {
-      if(url.indexOf('#') == -1) {
-        chrome.tabs.update(tabId, {url: url + "?wcmmode=disabled"});
-      } else {
-        var urlSplit = url.split("#");
-        chrome.tabs.update(tabId, {url: urlSplit[0] + "?wcmmode=disabled#" + urlSplit[1]});
-      }
+    } else if (mode == Mode.YELLOW) {
+      chrome.tabs.update(tabId, { url: url.replace(/\/cf#|\/editor\.html/gi, '') + "?wcmmode=disabled" });
     } else {
-      chrome.tabs.update(tabId, {url: url + "&wcmmode=disabled"});
+      chrome.tabs.executeScript({
+        code: 'document.querySelectorAll(\'[data-layer="Preview"]\')[0].click()'
+      });
     }
-  } else if (mode == Mode.GREEN) {
-    chrome.tabs.update(tabId, { url: url.replace(/\/cf#|\/editor\.html/gi, '') });
-  } else {
-    setPageActionTitle(tabId);
-  }
+  });
+//   if (mode == Mode.RED) {
+//     var key = getAuthoringUIModeKey(tabId);
+//     chrome.storage.sync.get({
+//       defaultAuthoringUIMode: AuthoringUIMode.CLASSIC
+//     }, function(items) {
+//       var defaultAuthoringUIMode = items.defaultAuthoringUIMode;
+//       chrome.storage.local.get(key, function(items) {
+//         var authoringUIMode = items[key];
+//         if (authoringUIMode == undefined) {
+//           authoringUIMode = defaultAuthoringUIMode;
+//         }
+//         var prefix = '/editor.html/content';
+//         if (authoringUIMode != AuthoringUIMode.TOUCH) {
+//           prefix = '/cf#/content';
+//         }
+//         url = url.replace('/content', prefix);
+//         url = url.replace(/&wcmmode=disabled|\?wcmmode=disabled/gi, '');
+//         chrome.tabs.update(tabId, { url: url });
+//       });
+//     });
+//   } else if (mode == Mode.YELLOW) {
+//     if(url.indexOf('?') == -1) {
+//       if(url.indexOf('#') == -1) {
+//         chrome.tabs.update(tabId, {url: url + "?wcmmode=disabled"});
+//       } else {
+//         var urlSplit = url.split("#");
+//         chrome.tabs.update(tabId, {url: urlSplit[0] + "?wcmmode=disabled#" + urlSplit[1]});
+//       }
+//     } else {
+//       chrome.tabs.update(tabId, {url: url + "&wcmmode=disabled"});
+//     }
+//   } else if (mode == Mode.GREEN) {
+// //     chrome.tabs.update(tabId, { url: url.replace(/\/cf#|\/editor\.html/gi, '') });
+//     // Granite.author.layerManager.loadLayer("Preview")
+//     // document.querySelectorAll('[data-layer="Edit"]')[0].click()
+//     chrome.tabs.executeScript({
+//       code: 'document.querySelectorAll(\'[data-layer="Edit"]\')[0].click()'
+//     });
+//   } else {
+//     setPageActionTitle(tabId);
+//   }
 });
 
 chrome.runtime.onStartup.addListener(function() {
